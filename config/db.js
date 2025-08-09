ca6c35aa3e5f369497268a18692db6e9936dcdd0
kkwenuja <wenujasl@gmail.com>
2025-08-10 05:19:49 +0530

    db:update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://wenujasl:ZzH8aHTbvMvGDvvk@cluster0.cqn6a.mongodb.net/?retryWrites=true&w=majority");
+        await mongoose.connect("mongodb+srv://wenujasl:C1kaQe7wgsLYSrak@supercluster.bh12x63.mongodb.net/?retryWrites=true&w=majority&appName=superCLuster");
         console.log("MongoDB connected");
     } catch (err) {
         console.error("MongoDB connection error:", err.message);
